7cbf5b50e668e8ffaf0cf541d4d686475ae991f4
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-04-22 14:43:56 +0600

    Primitive Types

---
+++ My Codes/Primitive_Types/Primitive_Types/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primitive_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+
+            // Int16
+            short s = 5;
+
+            // Int32
+            int i = 10;
+
+            // Int64
+            long l = 10000;
+
+            // float requires a f
+            float f = 2.0f;
+
+            double d = 50.0;
+
+            char ch = 'M';
+
+            bool flag = false;
+
+            const int c = 19;
+
+        }
+    }
+}
